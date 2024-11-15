4980b6d3976c4920158416d5972c18bc66c8d845
PANCHAMIIR96 <panchami.i.r96@gmail.com>
2024-11-15 16:25:23 -0500

    Homepage-admin

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,10 +1,23 @@
 function HomePage() {
+    const backgroundStyle = {
+        backgroundImage: 'url(https://wallpaperaccess.com/full/767352.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'white',
+        fontSize: '2rem',
+        fontWeight: 'bold',
+        textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)',
+    };
+
     return (
-        <>
-        <div>
-            Gourment Delights 
+        <div style={backgroundStyle}>
+            Welcome to Gourmet Delights Admin Portal
         </div>
-        </>
-    )
+    );
 }
+
 export default HomePage;
